317bac0a932ab2fdf0c37f796da53ddca04967b5
seif <seiftarek090@gmail.com>
2024-12-21 01:38:12 +0200

    Dividing the app into small modules to be easy to deal with it and to implement lazy-loading

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,33 @@
+import {NgModule} from "@angular/core";
+import {CommonModule} from "@angular/common";
+import {RouterModule} from "@angular/router";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+import {AlertComponent} from "./alert/alert.component";
+import {LoadingSpinnerComponent} from "./loading-spinner/loading-spinner.component";
+import {PlaceholderDirective} from "./placeholder/placeholder.directive";
+import {DropdownDirective} from "./dropdown.directive";
+
+@NgModule({
+  declarations: [
+    AlertComponent,
+    LoadingSpinnerComponent,
+    PlaceholderDirective,
+    DropdownDirective
+  ],
+  imports: [
+    CommonModule
+
+  ],
+  exports: [
+    CommonModule,
+    AlertComponent,
+    LoadingSpinnerComponent,
+    PlaceholderDirective,
+    DropdownDirective
+
+  ],
+
+})
+export class SharedModule {
+}
